1d8d0c379c0cd9b74f38695fa31b2eef4ab5fc28
Dueel <diego.bar@live.it>
2021-04-17 16:51:08 +0200

    Bonus 1 e 2

--- js/script.js
+++ js/script.js
@@ -3,44 +3,67 @@ function initVue(){
     new Vue ({
 
         el: '#app',
+
         data: {
 
-            posters: [],
+            albums: [],
             genres: [],
+
+            select: 'All'
         },
 
         methods: {
+            
+            getGenres: function(array){      
 
-            printGenre: function(){
+                array.forEach(item => {
 
-                const select = document.getElementById('select-genre');
-                const val = select.value
-                console.log(val);
-            }
-        },
-
-        mounted(){
-
-            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-            .then(data => {
-
-                data.data.response.forEach(item => {
-                    this.posters.push(item.poster);
+                    this.albums.push(item);
 
                     if (!this.genres.includes(item.genre)){
 
-                        this.genres.push(item.genre)
+                        this.genres.push(item.genre);
                     }
                 })
-            })
+            },
 
-            const select = document.getElementById('select-genre');
-            const val = select.value
-            console.log(select, val);
+            selectGenres: function(array){
 
-        }
+                const tagSelect = document.getElementById('select-genre');
+
+                tagSelect.addEventListener('change', ()=>{
+
+                    if (tagSelect.value == 'All'){
+
+                        this.albums = [...array];
+                    } else {
+                        
+                        this.albums = array.filter(item => item.genre == tagSelect.value);
+                    }
+                    
+                })         
+            }
+
+        },
+        
+        mounted(){
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(data => {
+
+                data.data.response.sort(function (a, b) {
+                    return a.year - b.year;
+                });
+                
+                this.getGenres(data.data.response);
+                this.selectGenres(data.data.response);
+            });
+
+        },
 
     })
 }
 
 document.addEventListener('DOMContentLoaded', initVue)
+
+
